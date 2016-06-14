5f4be300f826db9278728db7fdf77dd2ae15ab1a
正权 徐 <xuzhengquan@yundianjia.com>
2016-06-14 10:31:38 +0800

    临时提交

---
+++ AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by 权仔 on 16/6/14.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by 权仔 on 16/6/14.
+//  Copyright © 2016年 XZQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
